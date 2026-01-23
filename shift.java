13eeedc4b5069bf3e56cf12a2090b26e6bcb132e
SubaHariniB-dev <subaharinibalamuralikrishnan@gmail.com>
2026-01-23 18:29:19 +0530

    Add shift.java program

---
+++ shift.java
@@ -0,0 +1,14 @@
+class shift {
+    public static void main(String[] args){
+     int a =943;
+     System.out.println(a<<3);
+     int b=2593;
+     System.out.println(b>>6);
+     System.out.println(b<<2);
+      
+
+
+
+
+        }
+    }
