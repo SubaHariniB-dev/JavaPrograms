f6b0a9a547f3cd15235c137f789420b5473f2ba3
Suba Harini B <subaharinibalamuralikrishnan@gmail.com>
2026-01-23 22:10:17 +0530

    Add perimeter.java

---
+++ perimeter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class perimeter{
+    public static void main(String[] args){
+ Scanner sc =new Scanner(System.in);
+ System.out.print("enter the value of a, b, c:");
+ float a =sc.nextFloat();
+  float b =sc.nextFloat();
+   float c =sc.nextFloat();
+   float perimeter=a+b+c;
+   System.out.println("perimeter of a triangle="+ perimeter);
+   
+ 
+}
+ 
+}
+
+
+
