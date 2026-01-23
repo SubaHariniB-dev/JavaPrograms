0d61b3486d08863f1c1e7036e7181f38635c3265
SubaHariniB-dev <subaharinibalamuralikrishnan@gmail.com>
2026-01-23 18:28:10 +0530

    Add add two number.java

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class add {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int a,b,c;
+        System.out.print("enter two nuber:");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("sum is:"+c);
+    
+       }
+    }
