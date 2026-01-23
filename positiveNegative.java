87865bacd1fd22117cb3868b40c4fffa9216eba9
SubaHariniB-dev <subaharinibalamuralikrishnan@gmail.com>
2026-01-23 18:30:13 +0530

    Add positive_negative.java

---
+++ positiveNegative.java
@@ -0,0 +1,8 @@
+class positiveNegative {
+    public static void main(String[] args){
+      int a = 14;
+      String res =( (a==0)?"zero":(a>0)?"positive":"negative");
+      System.out.println(res);
+
+        }
+    }
