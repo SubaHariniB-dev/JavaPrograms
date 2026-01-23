a59248fd561ad9954f4cb0f548eb80b30437614c
SubaHariniB-dev <subaharinibalamuralikrishnan@gmail.com>
2026-01-23 18:25:46 +0530

    Add vote.java program

---
+++ vote.java
@@ -0,0 +1,10 @@
+class vote {
+    public static void main(String[] args){
+        int age = 17;
+        String res = ((age>=18)?"major":"minor");
+        System.out.println(res);
+
+
+
+        }
+    }
